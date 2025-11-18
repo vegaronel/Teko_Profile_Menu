12b0f52c91eb7a63914c2b7494d704463c06633f
joaquincuenca <146710300+joaquincuenca@users.noreply.github.com>
2025-11-18 10:09:23 +0800

    ✨ New material theme
    Please merge this ASAP!

--- app/src/main/java/com/learn/learnviewmodel/MainActivity.kt
+++ app/src/main/java/com/learn/learnviewmodel/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,27 +38,51 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun GreetingPreview(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Box(
             modifier = modifier
                 .size(200.dp)
-                .background(MaterialTheme.colorScheme.primary)
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
         ) {
-            Text("Hello Kotlin")
+            Text(
+                "Hello Kotlin",
+                style = MaterialTheme.typography.titleMedium,
+            )
         }
         Box(
             modifier = modifier
                 .size(200.dp)
-                .background(MaterialTheme.colorScheme.onPrimary)
+                .background(MaterialTheme.colorScheme.onPrimary),
+            contentAlignment = Alignment.Center
         ) {
-            Text("Hello Kotlin")
+            Column {
+                Text(
+                    "Hello Kotlin",
+                    style = MaterialTheme.typography.titleMedium,
+
+                    )
+                Text(
+                    "Hello Kotlin",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
         }
         Box(
             modifier = modifier
                 .size(200.dp)
-                .background(MaterialTheme.colorScheme.error)
+                .background(MaterialTheme.colorScheme.error),
+            contentAlignment = Alignment.Center
         ) {
-            Text("Hello Kotlin")
+            Text(
+                "Hello Kotlin",
+                style = MaterialTheme.typography.titleMedium,
+            )
         }
     }
 }
