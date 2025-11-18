90170ab2f3e112f62fb6ba743b7c9fa518d26608
Ronel Vega <ronelvega31@gmail.com>
2025-11-18 10:52:17 +0800

    ✨ Remove GreetingPreview composable from MainActivity

--- app/src/main/java/com/learn/learnviewmodel/MainActivity.kt
+++ app/src/main/java/com/learn/learnviewmodel/MainActivity.kt
@@ -4,21 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.learn.learnviewmodel.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,61 +16,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme(dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingPreview(modifier = Modifier.padding(innerPadding))
+
                 }
             }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun GreetingPreview(modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = modifier
-                .size(200.dp)
-                .background(MaterialTheme.colorScheme.primary),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                "Hello Kotlin",
-                style = MaterialTheme.typography.titleMedium,
-            )
-        }
-        Box(
-            modifier = modifier
-                .size(200.dp)
-                .background(MaterialTheme.colorScheme.onPrimary),
-            contentAlignment = Alignment.Center
-        ) {
-            Column {
-                Text(
-                    "Hello Kotlin",
-                    style = MaterialTheme.typography.titleMedium,
-
-                    )
-                Text(
-                    "Hello Kotlin",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.secondary
-                )
-            }
-        }
-        Box(
-            modifier = modifier
-                .size(200.dp)
-                .background(MaterialTheme.colorScheme.error),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                "Hello Kotlin",
-                style = MaterialTheme.typography.titleMedium,
-            )
         }
     }
 }
