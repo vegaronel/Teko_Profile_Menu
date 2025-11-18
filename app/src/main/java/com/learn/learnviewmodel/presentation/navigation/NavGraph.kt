66c0dfa7f614fd5ac6307ddcf04e226d74954991
Ronel Vega <ronelvega31@gmail.com>
2025-11-19 00:14:48 +0800

    ✨ Add ProfileScreen and update navigation

--- app/src/main/java/com/learn/learnviewmodel/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/learn/learnviewmodel/presentation/navigation/NavGraph.kt
@@ -2,26 +2,27 @@ package com.learn.learnviewmodel.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.learn.learnviewmodel.presentation.ui.screens.detailsscreen.DetailsScreen
-import com.learn.learnviewmodel.presentation.ui.screens.homescreen.HomeScreen
-import com.learn.learnviewmodel.presentation.viewmodel.DummyData
+import com.learn.learnviewmodel.presentation.ui.screens.homescreen.ProfileScreen
 
 @Composable
-fun NavGraph(modifier : Modifier = Modifier) {
+fun NavGraph(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
 
-    NavHost(modifier = modifier, navController = navController, startDestination = NavRoutes.Home.route, builder = {
-        composable(NavRoutes.Home.route) {
-            HomeScreen(navController)
-        }
-        composable(route = NavRoutes.Details.route) {
-            DetailsScreen()
-        }
-    })
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = NavRoutes.Home.route,
+        builder = {
+            composable(NavRoutes.Home.route) {
+                ProfileScreen()
+            }
+            composable(route = NavRoutes.Details.route) {
+                DetailsScreen()
+            }
+        })
 
 }
